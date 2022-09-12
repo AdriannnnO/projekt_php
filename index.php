94e5a06ac49c25cee664bfff48a906cc4bc7bbfd
AdriannnnO <95174036+AdriannnnO@users.noreply.github.com>
2022-09-12 09:56:51 +0200

    dymy w huj kurwa japierdole

--- index.php
+++ index.php
@@ -12,15 +12,15 @@
         <h1>Filmweb_logo</h1>
     </div>
     <div id="navbar">
-        <h3 id="navbarContent"><a href="s1.php">s1</h3></a>
-        <h3 id="navbarContent"><a href="s2.php">s2</h3></a>
+        <h3 id="navbarContent"><a href="s1.php">Filmy</h3></a>
+        <h3 id="navbarContent"><a href="s2.php">Ranking</h3></a>
         <h3 id="navbarContent"> <a href="O_nas.php">O nas</h3></a>
     </div>
     <div id="text">
         <P id="lorem">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In varius eleifend lobortis. Integer commodo sit amet nibh sit amet vehicula. Mauris ac tincidunt ante, id placerat tortor. Vestibulum dignissim sollicitudin erat quis vehicula. Integer nec ullamcorper mauris. Morbi tincidunt, tortor vel rutrum dictum, tellus tellus dapibus dui, non vehicula augue mauris at arcu. Sed laoreet tellus quis arcu pretium hendrerit. Phasellus posuere, metus nec elementum interdum, mauris velit accumsan enim, ut iaculis massa neque ut nisl. Quisque eu egestas mauris. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris tempus, velit eu gravida accumsan, orci tellus varius nisi, in euismod.</p>
     </div>
     <div id="Sponsorowane_filmy">
-        <h2>POLECANE FILMY</h2>
+        <h2>$PON$OROWANE FILMY</h2>
         <p>jakis film</p>
         <p>jakis film</p>
         <p>jakis film</p>
