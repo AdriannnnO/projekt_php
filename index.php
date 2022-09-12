cbae70b32ce2aa2bffa487224e6b8114f01d869b
AdriannnnO <95174036+AdriannnnO@users.noreply.github.com>
2022-09-12 09:31:13 +0200

    pierwszy comit
    xd

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="jd.css">
+    <title>jd</title>
+</head>
+<body>
+    <div id="stronga_główna">
+        <a href="index.php"><h1>Srona główna</h1></a>
+    </div>
+    <div id="navbar">
+        <h3 id="navbarContent"><a href="s1.php">s1</h3></a>
+        <h3 id="navbarContent"><a href="s2.php">s2</h3></a>
+        <h3 id="navbarContent"> <a href="O_nas.php">O nas</h3></a>
+    </div>
+    <div id="text">
+        <P id="lorem">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In varius eleifend lobortis. Integer commodo sit amet nibh sit amet vehicula. Mauris ac tincidunt ante, id placerat tortor. Vestibulum dignissim sollicitudin erat quis vehicula. Integer nec ullamcorper mauris. Morbi tincidunt, tortor vel rutrum dictum, tellus tellus dapibus dui, non vehicula augue mauris at arcu. Sed laoreet tellus quis arcu pretium hendrerit. Phasellus posuere, metus nec elementum interdum, mauris velit accumsan enim, ut iaculis massa neque ut nisl. Quisque eu egestas mauris. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris tempus, velit eu gravida accumsan, orci tellus varius nisi, in euismod.</p>
+    </div>
+    <div id="Sponsorowane_filmy">
+        <h2>POLECANE FILMY</h2>
+        <p>lorem ipsu blyblybly</p>
+        <p>lorem ipsu blyblybly</p>
+        <p>lorem ipsu blyblybly</p>
+        <p>lorem ipsu blyblybly</p>
+        <p>lorem ipsu blyblybly</p>
+    </div>
+
+    <?php
+                       
+    ?>
+</body>
+<footer>
+    <div id="footer_content">siema eniu</div>
+    <div id="footer_content">siema eniu</div>
+    <div id="footer_content">siema eniu</div>
+</footer>
+</html>
