b11892f3a7132963fda31afb9033d29611812361
AdriannnnO <95174036+AdriannnnO@users.noreply.github.com>
2022-11-14 11:31:26 +0100

    NAPRAWA SESSION
    no

--- indexlogin.php
+++ indexlogin.php
@@ -1,3 +1,6 @@
+<?php
+session_start();
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -9,7 +12,6 @@
 </head>
 <body>
 <?php
-        session_start();
         if (isset($_SESSION['username'])) {
             echo '<h1>Witaj ' . $_SESSION['username'] . '</h1>';
         }
